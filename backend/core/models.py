14bc5f344c5d186bd8ab7ad13cb4433da1ae8ed4
MatthewJRayner <raynerjmatthew@gmail.com>
2025-11-07 18:56:48 +0000

    Prepare backend for Railway deploy

--- backend/core/models.py
+++ backend/core/models.py
@@ -485,13 +485,7 @@ class Film(AbstractMedia):
     @classmethod
     @transaction.atomic
     def create_with_universal_item(cls, tmdb_data):
-        runtime = None
         runtime_value = tmdb_data.get("runtime")
-        if runtime_value is not None:
-            try:
-                runtime = timedelta(minutes=int(runtime_value))
-            except (ValueError, TypeError):
-                runtime = None
         
         director_data = next(
             (c for c in tmdb_data.get("credits", {}).get("crew", []) if c["job"] == "Director"),
@@ -519,7 +513,7 @@ class Film(AbstractMedia):
                 "alt_title": tmdb_data.get("original_title") if tmdb_data.get("original_title") != tmdb_data.get("title") else None,
                 "creator_string": director_name,
                 "alt_creator_name": alt_name,
-                "runtime": runtime,
+                "runtime": runtime_value,
                 "cast": [
                     {"name": c.get("name"), "role": c.get("character") or ""}
                     for c in tmdb_data.get("credits", {}).get("cast", [])
